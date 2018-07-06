966939f0a1db6633d8e806d3d81a22c341b59e90
Kazurista <ny77friends@gmail.com>
2018-07-06 13:12:46 +0900

    Create a Table View

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -19,7 +19,7 @@ import UIKit
         }
     }
     
-    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 39.0) {
         didSet {
             setupButtons()
         }
